6c677b03ec0d0211bb028f26bdc271276d04e5ac
Awais Ali <48981710+awais-malik716@users.noreply.github.com>
2019-07-02 17:43:15 +0500

    50% completed

---
+++ script.js
@@ -0,0 +1,15 @@
+function showMenu(){
+    document.getElementById('programs').style.display = 'block';
+}
+
+function hideProgram(){
+    document.getElementById('programs').style.display = 'none';    
+}
+
+document.getElementById("date").innerHTML = Date();
+
+// function showInput(){
+//     document.getElementById('search-input').style.display = "inline-block";
+//     document.querySelector('.bottom-buttons').style.display = "none";
+//     document.getElementById('bottom-button-2').style.display  "none";
+// }
